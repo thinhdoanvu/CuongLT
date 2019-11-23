c298e52a79b1564a36edc0521e49ce6381bfd829
THINH DOAN VU <thinhdv@ntu.edu.vn>
2019-11-23 10:16:59 +0700

    Add files via upload

---
+++ Annotation/rename.sh
@@ -0,0 +1,8 @@
+mv 	Korea.csv	Korea.csv
+mv Nocardia\ seriolae\ KH_11.csv KH_11.csv
+mv Nocardia\ seriolae\ KH_21.csv KH_21.csv
+mv Nocardia\ seriolae\ NT_50.csv NT_50.csv
+mv Nocardia\ seriolae\ PY_31.csv PY_31.csv
+mv Nocardia\ seriolae\ PY_37.csv PY_37.csv
+mv Nocardia\ seriolae\ VT_45.csv VT_45.csv
+mv Nocardia\ seriolae\ VT_62.csv VT_62.csv
